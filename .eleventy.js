17c893de7995a6c7a697709fe52fd00209c4f0cc
Adam Laki <info@adamlaki.com>
2023-04-20 11:35:15 +0200

    Add inline SVG shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
+
+const { parse, stringify } = require('himalaya');
 const htmlMinTransform = require('./src/transforms/html-min-transform.js');
+const Image = require('@11ty/eleventy-img');
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = config => {
@@ -7,6 +10,21 @@ module.exports = config => {
   config.addPassthroughCopy('./src/css/');
   config.addPassthroughCopy('./src/js/');
   config.addPassthroughCopy('./src/font/');
+
+  config.addNunjucksAsyncShortcode('svgIcon', async (src, cls) => {
+    const metadata = await Image(src, {
+      formats: ['svg'],
+      dryRun: true,
+    });
+    const svg = parse(metadata.svg[0].buffer.toString());
+
+    svg[0].attributes.push({
+      key: 'class',
+      value: cls ? cls : 'icon'
+    });
+
+    return stringify(svg);
+  })
 
   if (isProduction) {
     config.addTransform('htmlmin', htmlMinTransform);
