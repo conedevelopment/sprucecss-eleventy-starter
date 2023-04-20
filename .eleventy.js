28c22f241fa8200f258b108ef0648972e39f11bd
Adam Laki <info@adamlaki.com>
2023-04-20 09:38:59 +0200

    Add HTML minification if production

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,16 @@
+require('dotenv').config();
+const htmlMinTransform = require('./src/transforms/html-min-transform.js');
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = config => {
   config.addPassthroughCopy('./src/img/');
   config.addPassthroughCopy('./src/build/');
   config.addPassthroughCopy('./src/js/');
   config.addPassthroughCopy('./src/font/');
+
+  if (isProduction) {
+    config.addTransform('htmlmin', htmlMinTransform);
+  }
 
   return {
     markdownTemplateEngine: 'njk',
