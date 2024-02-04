3648bd2978aff145c4127265182964580c33354f
Adam Laki <info@adamlaki.com>
2024-02-04 09:58:51 +0100

    Updates

--- src/js/accordion-card.js
+++ src/js/accordion-card.js
@@ -1,27 +1,39 @@
 (() => {
   document.querySelectorAll('.accordion-card').forEach((accordion) => {
     const heading = accordion.querySelector('.accordion-card__title');
+    const button = document.createElement('button');
+    const icon = document.createElement('span');
 
-    accordion.classList.add('accordion-card--js');
+    button.classList.add('accordion-card__toggle');
+    button.setAttribute('aria-expanded', 'false');
 
-    heading.nextElementSibling.hidden = true;
-    heading.innerHTML = `
-      <button class="accordion-card__toggle" aria-expanded="false">
-        ${heading.textContent}
-        <svg aria-hidden="true" focusable="false" height="18px" viewBox="0 0 18 18" width="18px">
-          <path d="M13.5,8.625c0,-0.207 -0.168,-0.375 -0.375,-0.375l-8.25,0c-0.207,0 -0.375,0.168 -0.375,0.375l0,0.75c0,0.207 0.168,0.375 0.375,0.375l8.25,0c0.207,0 0.375,-0.168 0.375,-0.375l0,-0.75Z" style="fill:currentColor;"/>
-          <path class="vertical-line" d="M9.375,13.5c0.207,-0 0.375,-0.168 0.375,-0.375l0,-8.25c0,-0.207 -0.168,-0.375 -0.375,-0.375l-0.75,0c-0.207,0 -0.375,0.168 -0.375,0.375l-0,8.25c-0,0.207 0.168,0.375 0.375,0.375l0.75,-0Z" style="fill:currentColor;"/>
-        </svg>
-      </button>
+    icon.classList.add('accordion-card__icon');
+    icon.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+        <line class="vertical-line" x1="12" y1="5" x2="12" y2="19"></line>
+        <line class="horizontal-line" x1="5" y1="12" x2="19" y2="12"></line>
+      </svg>
     `;
 
-    const btn = heading.querySelector('button');
+    button.innerHTML = `${heading.textContent} ${icon.outerHTML}`;
+    heading.innerHTML = '';
+    heading.appendChild(button);
 
-    btn.addEventListener('click', () => {
-      const expanded = btn.getAttribute('aria-expanded') === 'true';
+    button.addEventListener('click', () => {
+      document.querySelectorAll('.accordion-card').forEach((otherAccordion) => {
+        const otherButton = otherAccordion.querySelector('button');
+        const otherContent = otherAccordion.querySelector('.accordion-card__content');
 
-      btn.setAttribute('aria-expanded', !expanded);
-      heading.nextElementSibling.hidden = expanded;
+        if (otherButton !== button) {
+          otherButton.setAttribute('aria-expanded', 'false');
+          otherContent.setAttribute('aria-hidden', 'true');
+        }
+      });
+
+      const expanded = button.getAttribute('aria-expanded') === 'true';
+
+      button.setAttribute('aria-expanded', !expanded);
+      heading.nextElementSibling.setAttribute('aria-hidden', expanded);
     });
   });
 })();
